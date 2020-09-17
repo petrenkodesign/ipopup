dadcd0469b1c7227d11bf6360361edd937506866
Alex Petrenko <petrenkodesign@gmail.com>
2020-09-17 14:14:52 +0300

    Create popup element

--- ipopup.js
+++ ipopup.js
@@ -1,4 +1,5 @@
 var debug = true;
+var popConfig = {heder:"This is popup", content:"Hello there! I am yor pop bro!", bgColor:"#fff"};
 
 window.onload = function() {
     // add link to style script on the site
@@ -19,11 +20,18 @@ window.onload = function() {
         popBody.setAttribute("class", "overlay");
         popBody.setAttribute("id", "popup1");
     var popContent = '<div class="popup">';
-        popContent +='<h2>Popup heder</h2>';
+        popContent +='<h2>'+popConfig.heder+'</h2>';
         popContent +='<a class="close" href="#">&times;</a>';
-        popContent +='<div class="content">Popup body here.</div>';
+        popContent +='<div class="content">'+popConfig.content+'</div>';
         popContent +='</div>';
         popBody.innerHTML = popContent;
     document.getElementsByTagName("body")[0].appendChild(popBody);
 }
 // end of document load
+
+// start configuration function, use after main script loaded
+function IpopConfig(popHeder, popContent, bgColor) {
+  popConfig.heder = popHeder;
+  popConfig.content =popContent;
+  popConfig.bgColor = bgColor;
+}
