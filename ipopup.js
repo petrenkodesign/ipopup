371443e4019dbd881d91b960ebc8e726ff6df654
Alex Petrenko <petrenkodesign@gmail.com>
2020-09-17 13:15:35 +0300

    Upload module structure

--- ipopup.js
+++ ipopup.js
@@ -6,10 +6,9 @@ if(document.createStyleSheet) {
   if(debug) console.log("Create link to CSS with createStyleSheet");
 }
 else {
-  var style = "@import url('https://petrenkodesign.github.io/ipopup/css/main.css');";
   var css_element = document.createElement('link');
   css_element.rel='stylesheet';
-  css_element.href='data:text/css,'+escape(style);
+  css_element.href='https://petrenkodesign.github.io/ipopup/css/main.css';
   document.getElementsByTagName("head")[0].appendChild(css_element);
 }
 
