8516f69c52779ec16911dcbd6332da294e3bc01d
Alex Petrenko <petrenkodesign@gmail.com>
2020-09-17 18:01:29 +0300

    Add user reaction algorithm

--- ipopup.js
+++ ipopup.js
@@ -4,49 +4,99 @@ var popConfig = {
   content:"Hello there! I am yor pop, bro!)",
   bgColor:"#fff",
   bgUrl:"https://petrenkodesign.github.io/ipopup/img/bg.jpg",
-  button: true
+  button: true,
+  show: true,
+  showtimes: 3
 };
 
 window.onload = function() {
-    // add link to style script on the site
-    if(document.createStyleSheet) {
-      document.createStyleSheet('https://petrenkodesign.github.io/ipopup/css/main.css');
-      if(debug) console.log("Create link to CSS with createStyleSheet");
-    }
-    else {
-      var css_element = document.createElement('link');
-      css_element.rel='stylesheet';
-      css_element.href='https://petrenkodesign.github.io/ipopup/css/main.css';
-      document.getElementsByTagName("head")[0].appendChild(css_element);
-      if(debug) console.log("Create link to CSS with createElement");
-    }
+    // cheking if popup status off
+    statusPop();
 
-    // create popup append body
-    var popBody = document.createElement("div");
-        popBody.setAttribute("class", "overlay");
-        popBody.setAttribute("id", "ipopup");
-    var popContent = '<div class="popup" id="ipopContent" style="background-color:'+popConfig.bgColor+'">';
-        popContent +='<h2>'+popConfig.heder+'</h2>';
-        popContent +='<a class="close" href="#">&times;</a>';
-        popContent +='<div class="content">'+popConfig.content+'</div>';
-        popContent +='</div>';
-        popBody.innerHTML = popContent;
-    document.getElementsByTagName("body")[0].appendChild(popBody);
+    if (popConfig.show) {
 
-    // chek configuration and customize ipopup
+        // add link to style script on the site
+        if(document.createStyleSheet) {
+          document.createStyleSheet('https://petrenkodesign.github.io/ipopup/css/main.css');
+          if(debug) console.log("Create link to CSS with createStyleSheet");
+        }
+        else {
+          var css_element = document.createElement('link');
+          css_element.rel='stylesheet';
+          css_element.href='https://petrenkodesign.github.io/ipopup/css/main.css';
+          document.getElementsByTagName("head")[0].appendChild(css_element);
+          if(debug) console.log("Create link to CSS with createElement");
+        }
 
-    // add custom styles to popup
-    if (popConfig.bgUrl!='none') document.getElementById("ipopContent").style.cssText += '; background-image: url('+popConfig.bgUrl+'); background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+        // create popup append body
+        var popBody = document.createElement("div");
+            popBody.setAttribute("class", "overlay");
+            popBody.setAttribute("id", "ipopup");
+        var popContent = '<div class="popup" id="ipopContent" style="background-color:'+popConfig.bgColor+'">';
+            popContent +='<h2>'+popConfig.heder+'</h2>';
+            popContent +='<a id="closepop" href="#">&times;</a>';
+            popContent +='<div class="content">'+popConfig.content+'</div>';
+            popContent +='</div>';
+            popBody.innerHTML = popContent;
+        document.getElementsByTagName("body")[0].appendChild(popBody);
+        document.getElementById("closepop").onclick = function() { closePop() };
 
-    // add start button
-    if (popConfig.button==true) addButton();
-}
-// end of document load
+        // chek configuration and customize ipopup
 
+        // add custom styles to popup
+        if (popConfig.bgUrl!='none') document.getElementById("ipopContent").style.cssText += '; background-image: url('+popConfig.bgUrl+'); background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+
+        // add start button
+        if (popConfig.button==true) addButton();
+
+    } // end of cheking popup showin status
+
+} // end of document load
+
+// function add on site start button for popup
 function addButton() {
   var popButton = document.createElement("a");
       popButton.setAttribute("class", "popButton");
       popButton.setAttribute("href", "#ipopup");
       popButton.innerHTML = '<img src="https://petrenkodesign.github.io/ipopup/img/icon.png">';
   document.getElementsByTagName("body")[0].appendChild(popButton);
+}
+
+// function count of how many time close ipopup
+function statusPop() {
+  var statusipop = getCookie('statusipop');
+  if (statusipop=='off') popConfig.show = false;
+}
+
+function closePop() {
+  var numofclose = getCookie('ncipop');
+  if (numofclose >  popConfig.showtimes) {
+    setCookie('statusipop', 'off');
+    statusPop();
+  }
+  else {
+    if(numofclose==false) numofclose = 0;
+    numofclose++;
+    setCookie('ncipop', numofclose);
+  }
+}
+
+// get cookie by name
+function getCookie(name) {
+    var cookies, _one_cookie;
+    cookies = document.cookie.split(';');
+    for (var i=0; i < cookies.length; i++) {
+        one_cookie = cookies[i].split('=');
+        if (one_cookie[0] == name) return one_cookie[1];
+    }
+    return false;
+}
+// set cookie, simply function
+function setCookie(name, value, exdays) {
+    var d, expires;
+    exdays = exdays || 3650;
+    d = new Date();
+    d.setTime(d.getTime() + (exdays2460601000));
+    expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires;
 }
