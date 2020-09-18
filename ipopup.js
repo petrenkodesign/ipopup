e3bc823e54e439f762e983385d5cdd6dd11a4af9
Alex Petrenko <petrenkodesign@gmail.com>
2020-09-18 16:54:07 +0300

    Upload for dobavkipop

--- ipopup.js
+++ ipopup.js
@@ -39,8 +39,8 @@ window.onload = function() {
             popBody.setAttribute("id", "ipopup");
             popBody.hidden = true; //crank
         var popContent = '<div class="popup" id="ipopContent" style="background-color:'+popConfig.bgColor+'">';
-            popContent +='<h2>'+popConfig.heder+'</h2>';
             popContent +='<a id="closepop" class="closepop" href="#">&times;</a>';
+            popContent +='<h2>'+popConfig.heder+'</h2>';
             popContent +='<div class="content">'+popConfig.content+'</div>';
             popContent +='</div>';
             popBody.innerHTML = popContent;
